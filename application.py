f390847c37d10ee4c68d7d4d22b0527d049ff296
srinivasyadav249 <95058213+srinivasyadav249@users.noreply.github.com>
2021-12-03 11:01:21 +0530

    Update application.py

--- application.py
+++ application.py
@@ -3,13 +3,13 @@ from flask_restful import Resource, Api
 
 app=Flask(__name__)
 api=Api(app)
-
+app_port = 8080
 application_info = {
     "service_name": "myapplication",
     "version": "1.0.0",
     "git_commit_sha": "abc57858585",
     "environment": {
-    "service_port": "8080",
+    "service_port": app_port,
     "log_level": "INFO"
     }
     }
@@ -18,5 +18,4 @@ class Application(Resource):
         return application_info
 
 api.add_resource(Application,"/info")
-app.run(port=8080,host="0.0.0.0")
-
+app.run(port=app_port,host="0.0.0.0")
